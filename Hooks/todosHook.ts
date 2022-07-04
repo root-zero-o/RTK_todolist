28485e2f5a446cd980d1f6bfdc9d76eb82baa185
RootZero <rootzero17@gmail.com>
2022-07-04 23:47:58 +0900

    오류 해결 ~!

--- Hooks/todosHook.ts
+++ Hooks/todosHook.ts
@@ -1,17 +1,21 @@
+import { TodoState } from "./../type";
+import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/modules";
 import { fetchTodos } from "../store/modules/todosSlice";
 
+type AppDispatch = ThunkDispatch<TodoState, any, AnyAction>;
+
 export default function useTodos() {
+  const dispatch: AppDispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchTodos());
-    },[dispatch]) 
-
-    const { lists, loading, error }  = useSelector(( state : RootState ) => state.todos)
-    return {lists, loading, error}
+  const { lists, loading, error } = useSelector(
+    (state: RootState) => state.todos
+  );
+  return { lists, loading, error };
 }
-
